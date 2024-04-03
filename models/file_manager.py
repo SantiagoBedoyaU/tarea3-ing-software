efddbb5f4e1d0516c782084c14c4623d3e5946d9
SantiagoBedoya <a.sbedoya200110@gmail.com>
2024-04-02 21:53:12 -0500

    Adding comments to classes and methods

--- models/file_manager.py
+++ models/file_manager.py
@@ -5,10 +5,15 @@ from .student import Student
 
 
 class FileManager:
+    """Se encarga de leer y escribir los archivos que contienen a los estudiantes"""
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
 
     def read_csv(self) -> list[Student]:
+        """
+        Lee al archivo .csv, es proceso por el paquete csv
+        y retorna la lista de estudiantes que encontro en este archivo
+        """
         try:
             students = []
             with open(self.file_path) as file:
@@ -21,6 +26,7 @@ class FileManager:
             raise FileNotFoundError(f"el archivo {self.file_path} no existe")
 
     def write_json(self, students: list[Student]):
+        """Escribe una lista de estudiantes en un archivo json"""
         try:
             data = [student.to_dict() for student in students]
             json_content = json.dumps(data, ensure_ascii=False)
--- models/student.py
+++ models/student.py
@@ -1,10 +1,12 @@
 class Student:
+    """Encargada de la estructura de la informacion de un estudiante"""
     def __init__(self, id: str, name: str, lastname: str) -> None:
         self.id = id
         self.name = name
         self.lastname = lastname
 
     def to_dict(self) -> dict[str, str]:
+        """Retorna la informacion de un estudiante en forma de un diccionario"""
         return {
             "id": self.id,
             "nombre": self.name,
